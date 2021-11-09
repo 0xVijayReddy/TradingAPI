d7ec0a88d9afacd0e98c21e5163de3281bb80eb1
Vijay Reddy <vijayreddy@Vijays-MacBook-Air.local>
2021-11-09 18:00:20 +0530

    testing heroku

--- routes/alerts/alerts-sendalert.js
+++ routes/alerts/alerts-sendalert.js
@@ -8,14 +8,14 @@ funcs.getNewToken().then(response=>{
 });
 
 
-router.get("/",async (req,res)=>{
+router.get("/", (req,res)=>{
 	models.Position.find()
 	.sort({option_type:1})
 	.then(data=>{
 		data = data.map(pos=>({exchange:"NFO",tradingsymbol:pos.symbol,symboltoken:pos.token}));
 		return data;
 	})
-	.then(async (data)=>{
+	.then(data=>{
 		const request1 = axios(funcs.getConfig(token,data[0]));
 		const request2 = axios(funcs.getConfig(token,data[1]));
 		axios.all([request1,request2])
@@ -23,16 +23,16 @@ router.get("/",async (req,res)=>{
 			if(prices[0].status){
 				const call_option_price = prices[0].data.data.ltp;
 				const put_option_price = prices[1].data.data.ltp;
-				res.send({call:call_option_price,put:put_option_price});
 				const percent = Math.round((Math.min(call_option_price,put_option_price)/Math.max(call_option_price,put_option_price))*100);
 				if(percent>50){
 					if(call_option_price>put_option_price){
-						axios.post(process.env.SLACK_WEBHOOK_URL,{text:"Rise alert"+percent});
-
+						console.log("alert sent");
+						axios.post(process.env.SLACK_WEBHOOK_URL,{text:"Rise alert "+percent});
 
 					}
 					else{
-
+						console.log("alert sent");
+						axios.post(process.env.SLACK_WEBHOOK_URL,{text:"Fall alert "+percent});
 					}
 				}
 			}
@@ -40,7 +40,9 @@ router.get("/",async (req,res)=>{
 		})
 
 	})
-	res.send("fcuk");
+	.then(_=>{
+		res.sendStatus(200);
+	})
 	
 })
 
