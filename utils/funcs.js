d0f0bb7169eeafe1a7a38ebd7396d6800e033a7e
Vijay Reddy <vijayreddy@Vijays-MacBook-Air.local>
2021-11-10 23:04:56 +0530

    Changed alert messages

--- utils/funcs.js
+++ utils/funcs.js
@@ -52,13 +52,12 @@ sendAlert = (prices)=>{
 	const call_option_price = prices[0].data.data.ltp;
 	const put_option_price = prices[1].data.data.ltp;
 	const percent = Math.round((Math.min(call_option_price,put_option_price)/Math.max(call_option_price,put_option_price))*100);
-	console.log(call_option_price,put_option_price,percent);
 	if(percent<50){
 		if(call_option_price>put_option_price){
-			axios.post(process.env.SLACK_WEBHOOK_URL,payload={"text": "Alert from updated code"});
+			axios.post(process.env.SLACK_WEBHOOK_URL,payload={"text": `Adjust PE\n PE:${put_option_price} \n CE:${call_option_price} \n <https://www.google.com|Adjust here>`});
 		}
 		else{
-			axios.post(process.env.SLACK_WEBHOOK_URL,payload={"text": "Alert from updated code"});
+			axios.post(process.env.SLACK_WEBHOOK_URL,payload={"text": `Adjust CE\n CE:${call_option_price} \n PE:${put_option_price} \n <https://www.google.com|Adjust here>`});
 		}
 	}
 
